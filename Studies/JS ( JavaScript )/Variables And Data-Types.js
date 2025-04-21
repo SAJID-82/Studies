80b5e23a5902a0526b6be9ceaf86e16d0ff70395
Sajid-2007 <Mdshajid227@gmail.com>
2025-04-22 00:26:45 +0530

    Date 21-04-2025

--- Studies/JS ( JavaScript )/Variables And Data-Types.js
+++ Studies/JS ( JavaScript )/Variables And Data-Types.js
@@ -1,11 +1,49 @@
-console.log("Hello JavaScript !");
+// console.log("Hello JavaScript !");
 
 // Varianbles in JS
 
 // Variables are containers for data
 
-lastName = "SAJID"
+// var lastName = "SAJID";
 
-console.log(lastName)
+// console.log(lastName);
 
-// 37:00 / 1:21:10
+// this is assignment operator =,
+// assignment operator transfer right side variable value to left side variable.
+// Example a = b 
+//         |   |
+// left side    right side 
+
+
+// Variable rules
+
+// Variable names are case sensitive; "a" & "A" is Different.
+// Only letters digit, underscore(_) and $ is allowed. (not even space);
+// Only a letter, underscore(_) or $ should be 1st character.
+// Reserved words cannot be variable names.
+// Always write variable name in camel case. 
+
+
+// let, const & var
+
+// 1. var : Variable can be re-declared & updated.
+//  it is a global scope variable.
+
+// 2. let : Variable cannot be re-declared but can be updated.
+//  it is a block scope variable.
+
+// 3. const : Variable cannot be re-declared or updated.
+//  it is a block scope variable. 
+
+
+//  Data Types in JavaScript
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+// 7. Symbol
+
+// 57:00 / 1:21:10
