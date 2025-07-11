697486bb5ded99d8a0127b64b8e6b2ea9ce4857e
Sajid-2007 <Mdshajid227@gmail.com>
2025-07-11 20:56:14 +0530

    11-07-2025(1)

---
+++ Studies/Edabit Practice/Array/2. Easy/Day 1.js
@@ -0,0 +1,13 @@
+// Find the Smallest and Biggest Numbers
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+function minMax(arr){
+
+}
+
+// Examples
+console.log(minMax([1, 2, 3, 4, 5])) // ➞ [1, 5]
+
+console.log(minMax([2334454, 5])) // ➞ [5, 2334454]
+
+console.log(minMax([1])) // ➞ [1, 1]
